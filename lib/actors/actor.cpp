00c61b83a90efe9af79a4501ba7522285e618f5e
Wes <hntergren3@yahoo.com>
2012-12-08 21:32:44 -0500

    split actor into .h and .cpp...checking before I overwrite my .cpp by mistake when I forget the order of g++ arguments

---
+++ lib/actors/actor.cpp
@@ -0,0 +1,66 @@
+#include "actor.h"
+
+actor_t::actor_t(const std::string name, const hit_point_t max){
+	m_name = name;
+    m_max_hp = max;
+	m_cur_hp = max;
+}
+
+actor_t::actor_t(const actor_t& rhs){
+	m_name = rhs.m_name;
+	m_max_hp = rhs.m_max_hp;
+	m_cur_hp = m_cur_hp;
+}
+
+actor_t::hit_point_t cur_hp() const {
+	return m_cur_hp;
+}
+
+actor_t::hit_point_t& mutable_cur_hp() {
+	return &m_cur_hp;
+}
+
+actor_t::hit_point_t max_hp() const {
+	return m_max_hp;
+}
+
+std::string actor_tget_name() const {
+	return m_name;
+}
+
+void actor_t::set_max_hp(hit_point_t hp){
+	if(hp < 0) return;
+    set_stat(m_max_hp, hp, MIN_HP, MAX_HP);
+}
+
+void actor_t::inc_max_hp(hit_point_t amount){
+    set_max_hp(m_cur_hp + amount);
+}
+void actor_t::dec_max_hp(hit_point_t amount){
+	set_max_hp(m_cur_hp - amount);
+}
+
+void actor_t::set_cur_hp(hit_point_t hp){
+	set_stat(m_cur_hp, hp, MIN_HP, m_max_hp);
+}
+
+template <class T>
+void actor_t::set_stat(T& my_stat, const T in_stat, int min, int max){
+	in_stat > max ? my_stat = max : my_stat = min;
+	if(my_stat < min)  my_stat = min;
+}
+
+void actor_t::inc_cur_hp(hit_point_t amount){
+	set_cur_hp(m_cur_hp + amount);
+}
+void actor_t::dec_cur_hp(hit_point_t amount){
+	set_cur_hp(m_cur_hp - amount);
+}
+
+void actor_t::operator+(const hit_point_t amount){
+	inc_cur_hp(amount);
+}
+
+void actor_t::operator-(const hit_point_t amount){
+	dec_cur_hp(amount);
+}
